e1a316715cc0f2a07197c037f2345e7ea93922d7
braescurares <braescurares96@gmail.com>
2017-05-30 20:54:05 +0300

    deleting useless comments

--- src/servlet/AddProdusServlet.java
+++ src/servlet/AddProdusServlet.java
@@ -12,9 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-/**
- * Created by bujor on 30.05.2017.
- */
+
 @WebServlet(name = "AddProdusServlet", urlPatterns = {"/addProdus"})
 public class AddProdusServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- src/servlet/DoAddProdusServlet.java
+++ src/servlet/DoAddProdusServlet.java
@@ -14,9 +14,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-/**
- * Created by bujor on 30.05.2017.
- */
+
 @WebServlet(name = "DoAddProdusServlet", urlPatterns = {"/doAddProdus"})
 public class DoAddProdusServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
